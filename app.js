7ac62b0a0e27c2bdbf4287b99407ce4301566e3b
AlleyChang <yifanchang0602@gmail.com>
2022-08-23 15:02:30 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const routes = require('./routes/index')
 require('./config/mongoose')
 
@@ -11,12 +12,16 @@ const port = 3000
 //檔案設定
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'IAmUrSecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(routes)
-
-
 // server監聽設定
 app.listen(port, () =>{
   console.log(`it is running on http://localhost:${port}`)
