eca27d1b21420fbc5a9832334c0bfbbe88e7f191
AlleyChang <yifanchang0602@gmail.com>
2022-08-27 11:25:00 +0800

    feat: modify restaurantSeeder

--- models/restaurants.js
+++ models/restaurants.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
-  name: {type: String,
+  name: {
+    type: String,
     required: true },
   name_en: {
     type: String
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,18 +1,36 @@
-const mongoose = require('mongoose')
 const RestaurantList = require('../restaurants')
-const restaurants = require('../../restaurant')
+const restaurants = require('./restaurant').results //引入json檔
 const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
+const User = require('../users')
+const SEED_USER = require('./users').users
 
-
-
-db.once('open', () => {
-  console.log('MONGODB connected successfully!')
-  const restaurantsData = restaurants.results
-  
-  RestaurantList.insertMany(restaurantsData, (err, restaurant) => {
-    if (err) {
-      return console.error(err)
-    }     
-  })
-  console.log('done')
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+db.once('open', () => {    
+  Promise.all(
+    SEED_USER.map(user => {
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(user.password, salt))
+        .then(hash => User.create({
+          name: user.name,
+          email: user.email,
+          password: hash
+        }))
+        .then(user => {
+          return Promise.all(Array.from({ length: 3}, (_, i) => {
+            const item = restaurants[3-i]
+            item.userId = user._id
+            restaurants.splice(3-i, 1)
+            return RestaurantList.create(item)
+            }))
+        })   
+    })
+  ) 
+  .then(() => {
+    console.log('done')})
+  .catch(err => console.log('err'))
 })
+
